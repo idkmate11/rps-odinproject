f5f1b88616b4656de1f54bc294d282c6d9b58aef
idkmate11 <114446050+idkmate11@users.noreply.github.com>
2024-07-27 14:14:44 +0200

    Wrote logic for playing a single round

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -28,3 +28,37 @@ function getHumanChoice() {
     return "Scissors";
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    console.log("You Win! Rock beats Scissors!");
+    ++humanScore;
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    console.log("You Win! Paper beats Rock!");
+    ++humanScore;
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    console.log("You Win! Scissors beat Paper!");
+    ++humanScore;
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+    console.log("You Lose! Rock beats Scissors!");
+    ++computerScore;
+    console.log(computerScore);
+  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    console.log("You Lose! Paper beats Rock!");
+    ++computerScore;
+  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+    console.log("You Lose! Scissors beat Paper!");
+    ++computerScore;
+  } else if (computerChoice === humanChoice || humanChoice === computerChoice) {
+    console.log("It's a draw!!");
+    ++computerScore;
+    ++humanScore;
+  } else {
+    console.log("Wrong Input");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
